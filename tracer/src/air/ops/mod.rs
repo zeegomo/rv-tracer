bad8c858def133a19dfe2c641d5b3919a7757900
Giacomo Pasini <g.pasini98@gmail.com>
2023-12-18 12:03:58 +0100

    fix jal/jalr constraints

--- tracer/src/air/ops/mod.rs
+++ tracer/src/air/ops/mod.rs
@@ -25,31 +25,32 @@ air! {
 }
 
 // FIX: we ignore constraints for rd = 0 but we should not do that
+// h0, h1 overflow bits
+// h1 has to be removed because pc will be interpreted as unsigned
 air! {
     name = jal,
     opcode = "1101111",
     parse = rd,
     constraints =
         rd + h0 * E::from(1u64 << 32) - (current[PC] + E::from(4u32)) => 1,
-        (next[PC] + h1 * E::from(1u64 << 32)) - (current[PC] + jal_offset) => 1
+        (next[PC] - h1 * E::from(1u64 << 32)) - (current[PC] + jal_offset) => 1
 }
 
 // FIX: we ignore constraints for rd = 0 but we should not do that
 // h0, h1 overflow bits
 // overflow before or after adding h2?
 // TODO: h2 can be directly substituted in the second constraint
+// h1 has to be removed because pc will be interpreted as unsigned
 air! {
     name = jalr,
     opcode = "1100111",
     funct3 = "000",
     parse = rs1 / rd,
     constraints =
-        rd - (current[PC] + E::from(4u32)) => 1,
-        (next[PC] + h1 * E::from(1u64 << 32)) - (rs1 + simm - h2) => 1,
+        rd  + h0 * E::from(1u64 << 32) - (current[PC] + E::from(4u32)) => 1,
+        (next[PC] - h1 * E::from(1u64 << 32)) - (rs1 + simm) => 1,
         h2 - (current[RS1_BITS_END + 31] + current[IMM_END + 11] - E::from(2u32) * current[RS1_BITS_END + 31] * current[IMM_END + 11]) => 2
 }
-
-// (next[PC] + h1 * E::from(1u64 << 32)) - (current[PC] + jal_offset) => 1
 
 // // TODO: signed version
 // air!(
