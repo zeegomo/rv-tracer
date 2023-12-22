fb0099d1952c84761437420c6436fac5d1c297d1
Giacomo Pasini <g.pasini98@gmail.com>
2023-12-22 14:03:38 +0100

    add sparse memory

--- tracer/src/sim/memory.rs
+++ tracer/src/sim/memory.rs
@@ -3,7 +3,7 @@ use std::collections::BTreeMap;
 /// A simple `Memory` implementation, that creates an address space with just some DRAM.
 #[derive(Debug)]
 pub struct SimpleMemory {
-    pub dram: BTreeMap<u32, u8>,
+    pub dram: BTreeMap<u64, u8>,
 }
 
 impl SimpleMemory {
@@ -15,7 +15,7 @@ impl SimpleMemory {
 
     pub fn load_slice(&mut self, addr: u32, data: &[u8]) {
         for (i, byte) in data.iter().enumerate() {
-            self.dram.insert(addr + i as u32, *byte);
+            self.dram.insert(addr as u64 + i as u64, *byte);
         }
     }
 }
@@ -30,12 +30,13 @@ impl rvsim::Memory for SimpleMemory {
     fn access<T: Copy>(&mut self, addr: u32, access: rvsim::MemoryAccess<T>) -> bool {
         // TODO: not a fan of this api and this hack
         let size = std::mem::size_of::<T>();
-        let mut slice = (addr..addr + size as u32)
+        let addr = addr as u64;
+        let mut slice = (addr..addr + size as u64)
             .map(|addr| self.dram.get(&addr).copied().unwrap_or(0))
             .collect::<Vec<_>>();
         let res = slice.access(0, access);
         for (i, byte) in slice.into_iter().enumerate() {
-            self.dram.insert(addr + i as u32, byte);
+            self.dram.insert(addr + i as u64, byte);
         }
         res
     }
